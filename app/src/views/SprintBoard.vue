388b6fbc8e3498452cf99ae68f011345501b3293
Erin <colorandcontrast@gmail.com>
2020-08-13 23:52:24 -0300

    Implement moment

--- app/src/views/SprintBoard.vue
+++ app/src/views/SprintBoard.vue
@@ -1,13 +1,21 @@
 <template>
-    <div id="app">
-        <h1 v-if="!loading">{{ sprint.nickname }}:</h1>
+  <div id="app">
+    <h1 v-if="!loading">{{ sprint.nickname }}:</h1>
+    <p>
+      {{ displayDate(sprint.start_date) }} - {{ displayDate(sprint.end_date) }}
+    </p>
 
-        <button @click="createLane">Add lane</button>
+    <button @click="createLane">Add lane</button>
 
-        <div v-if="!loading" class="lanes">
-            <Lane v-for="lane in lanes" :key="lane.id" :lane="lane" :sprintId="sprint.id" />
-        </div>
+    <div v-if="!loading" class="lanes">
+      <Lane
+        v-for="lane in lanes"
+        :key="lane.id"
+        :lane="lane"
+        :sprintId="sprint.id"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -15,44 +23,55 @@
 // Render X number of lane components, + add lane button
 // also - allow edit sprint details here.
 // can assume all sprints have the same lanes.
+// CAN WE THOUGH? What happens to cards in a deleted lane?
+//    - delete
+//    - disappear... (no)
+//    - prevent deleting lane if has cards (might have cards in another sprint!!)
+//      - notify which sprint it has cards
+//    - move cards to 'uncategorized' lane
+
+import moment from "moment";
 
 import Lane from "../components/Lane";
 import { getters, actions } from "../utilities/store";
 
 export default {
-    name: "SprintBoard",
-    components: {
-        Lane,
+  name: "SprintBoard",
+  components: {
+    Lane,
+  },
+  data() {
+    return {
+      loading: true,
+      sprint: {},
+    };
+  },
+  computed: {
+    lanes() {
+      return getters.lanes();
     },
-    data() {
-        return {
-            loading: true,
-            sprint: {},
-        };
+  },
+  async created() {
+    const vm = this;
+    await actions.init(vm.$route.params.id);
+    vm.sprint = actions.getSprint(vm.$route.params.id);
+    vm.loading = false;
+  },
+  methods: {
+    createLane() {
+      actions.createLane("New lane", "FF0000");
     },
-    computed: {
-        lanes() {
-            return getters.lanes();
-        },
+    displayDate(date) {
+      return moment(date).format("MMM D");
     },
-    async created() {
-        const vm = this;
-        await actions.init(vm.$route.params.id);
-        vm.sprint = actions.getSprint(vm.$route.params.id);
-        vm.loading = false;
-    },
-    methods: {
-        createLane() {
-            actions.createLane("New lane", "FF0000");
-        },
-    },
+  },
 };
 </script>
 
 <style scoped>
 .lanes {
-    display: flex;
-    justify-content: space-between;
-    height: 100%;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
 }
 </style>
